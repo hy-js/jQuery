68f8c6d5bee2685c21d802c7397f2d615faba12d
RHYS <62794614+hy-js@users.noreply.github.com>
2022-04-07 16:48:27 +1000

    add select option

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,30 +1,50 @@
-// Movies -------------------------------
-$("#movies").hide();
-$.getJSON(BASE_URL + "/discover/movie", apiOptions)
-  .then((data) => {
-    // console.log(data);
-    // const results = data.results
-    const { results } = data;
-    const { page } = data;
-    console.log(results);
-    results.forEach((movie) => {
-      console.log(movie);
-      $("#movies").append(
-        `
-        <div class="col s12 m4 l3 movie-container">
-        <a href="/movies/${movie.id}">
-        <img src="${IMAGE_URL}${movie.poster_path}" class="responsive-img" alt="${movie.title}"/>
-        </a>
-        </div>
-        `
-      );
-    });
-    // Show movies
-    // Progress Progress
-    $(".preloader-wrapper").hide();
-    $("#movies").show();
-  })
+// On load
+$(document).ready(function () {
+  $("select").formSelect();
+  getMovies();
+});
 
-  .catch((err) => {
-    console.log(err);
+// Access sort values
+$("#sort").change(function () {
+  console.log($("#sort").val());
+  apiOptions["sort_by"] = $("#sort").val();
+  console.log(apiOptions);
+  getMovies();
+});
+
+//
+//
+
+// Movies -------------------------------
+function getMovies() {
+  $("#movies").empty();
+  $("#movies").hide();
+  $.getJSON(BASE_URL + "/discover/movie", apiOptions)
+    .then((data) => {
+      const { results } = data;
+      renderPages(results);
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function renderPages(results) {
+  results.forEach((movie) => {
+    console.log(movie);
+    $("#movies").append(
+      `
+      <div class="col s12 m4 l3 movie-container">
+      <a href="/movies/${movie.id}">
+      <img src="${IMAGE_URL}${movie.poster_path}" class="responsive-img" alt="${movie.title}"/>
+      </a>
+      </div>
+      `
+    );
   });
+  // Show movies
+  // Progress Progress
+  $(".preloader-wrapper").hide();
+  $("#movies").fadeIn();
+}
