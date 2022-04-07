ab8cd60cf4f7e8b630a010d75b77bdc24d9283fd
RHYS <62794614+hy-js@users.noreply.github.com>
2022-04-07 14:25:06 +1000

    create express server

---
+++ public/js/movies.js
@@ -0,0 +1,30 @@
+// Movies -------------------------------
+$("#movies").hide();
+$.getJSON(BASE_URL + "/discover/movie", apiOptions)
+  .then((data) => {
+    // console.log(data);
+    // const results = data.results
+    const { results } = data;
+    const { page } = data;
+    console.log(results);
+    results.forEach((movie) => {
+      console.log(movie);
+      $("#movies").append(
+        `
+        <div class="col s12 m4 l3 movie-container">
+        <a href="/movies/${movie.id}">
+        <img src="${IMAGE_URL}${movie.poster_path}" class="responsive-img" alt="${movie.title}"/>
+        </a>
+        </div>
+        `
+      );
+    });
+    // Show movies
+    // Progress Progress
+    $(".preloader-wrapper").hide();
+    $("#movies").show();
+  })
+
+  .catch((err) => {
+    console.log(err);
+  });
--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,3 @@
-console.log("Connected");
-
-$("h1").css("color", "teal");
-
-
-
 // Weather -------------------------------
 $("#weather").css("border", "1px solid black");
 
@@ -13,27 +7,27 @@ $("#weather").hide();
 $.ajax({
   url: "https://www.metaweather.com/api/location/1103816/",
   success: (data) => {
-    const { consolidated_weather } = data;
+    const { consolidated_weather: weather } = data;
     $("#weather").append(
       `
       <div id="icon">
-      <img src="https://www.metaweather.com/static/img/weather/${consolidated_weather[0].weather_state_abbr}.svg"/>
+      <img src="https://www.metaweather.com/static/img/weather/${weather[0].weather_state_abbr}.svg"/>
       </div>
       <h3>Weather in <strong>${data.title}</strong></h3>
       `
     );
     $("#weather").append(
       "<p><strong>" +
-        parseInt(consolidated_weather[0].the_temp) +
+        parseInt(weather[0].the_temp) +
         " degrees </strong> today</p>"
     );
     $("#weather").append(
       "<p><strong>" +
-        consolidated_weather[0].weather_state_name +
+        weather[0].weather_state_name +
         `</strong> are forecast</p>
         `
     );
-    //  consolidated_weather.forEach((day, index) => {
+    //  weather.forEach((day, index) => {
     //    $("#weather").append(
     //      `<p>
     //      Day ${index}: ${parseInt(day.the_temp)} degrees
@@ -47,36 +41,3 @@ $.ajax({
     $("#weather").show();
   }
 });
-
-// Movies -------------------------------
-let BASE_URL = "https://api.themoviedb.org/3/discover/movie";
-let IMAGE_URL = "https://image.tmdb.org/t/p/w300/";
-let API_KEY = "?api_key=12890aac4bd3d481725b4e373193a5bf&";
-
-$("#movies").hide();
-$.getJSON(`${BASE_URL}${API_KEY}`)
-  .then((data) => {
-    // console.log(data);
-    // const results = data.results
-    const { results } = data;
-    const { page } = data;
-    console.log(results);
-    results.forEach((movie) => {
-      $("#movies").append(
-        `
-        <div class="col s12 m4 l3">
-          <h4 class="truncate">${movie.title}</h4>
-          <img src="${IMAGE_URL}${movie.poster_path}" class="responsive-img" />
-        </div>
-        `
-      );
-    });
-    // Show movies
-    // Progress Progress
-    $(".preloader-wrapper").hide();
-    $("#movies").show();
-  })
-
-  .catch((err) => {
-    console.log(err);
-  });
