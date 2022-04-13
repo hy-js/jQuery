29fba1bfbed7dd07ec45f7bb7eb416461ef7b3e9
RHYS <62794614+hy-js@users.noreply.github.com>
2022-04-13 20:25:28 +1000

    update design

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,10 +1,13 @@
+let IMAGE_URL = "http://image.tmdb.org/t/p/w500";
+let page = 1;
+
 // On load
 $(document).ready(function () {
   $("select").formSelect();
-  getMovies();
+  getMovies(page);
 });
 
-// Access sort values
+// Sorting
 $("#sort").change(function () {
   console.log($("#sort").val());
   apiOptions["sort_by"] = $("#sort").val();
@@ -12,35 +15,57 @@ $("#sort").change(function () {
   getMovies();
 });
 
-//
-//
+// Pagination
+$("#nextPage").click(() => {
+  page++;
+  getMovies(page);
+});
+$("#prevPage").click(() => {
+  page--;
+  getMovies(page);
+});
+// Random button - select random page out of 100
+$("#random").click(() => {
+  page = Math.floor(Math.random() * (500 - 1)) + 1;
+  getMovies(page);
+});
 
 // Movies -------------------------------
-function getMovies() {
+function getMovies(page) {
   $("#movies").empty();
   $("#movies").hide();
-  $.getJSON(BASE_URL + "/discover/movie", apiOptions)
+  $.getJSON("/api/all-movies/" + page)
     .then((data) => {
       const { results } = data;
+      console.log(data);
+      $("#page-number").html(data.page);
       renderPages(results);
     })
-
     .catch((err) => {
       console.log(err);
+      $("#movies").append(
+        `
+      <div class="col s12 m4 l3 movie-container">
+      <h3> There was an error<h3>
+      <a href="/movies">Go Back</a>
+      </div>
+      `
+      );
+      $(".preloader-wrapper").hide();
+      $("#movies").fadeIn();
     });
 }
 
 function renderPages(results) {
   results.forEach((movie) => {
-    console.log(movie);
     $("#movies").append(
       `
-      <div class="col s12 m4 l3 movie-container">
-      <a href="/movies/${movie.id}">
-      <img src="${IMAGE_URL}${movie.poster_path}" class="responsive-img" alt="${movie.title}"/>
-      </a>
-      </div>
-      `
+    <div class="col s12 m4 l3 movie-container">
+    <a href="/movies/${movie.id}">
+    <img src="${IMAGE_URL}${movie.poster_path}" class="responsive-img" alt="${movie.title}"/>
+    </a>
+    </div>
+    `
     );
   });
   // Show movies
--- public/js/singleMovie.js
+++ public/js/singleMovie.js
@@ -1,6 +1,5 @@
 // Spider-man
 // const movie_id = "634649";
-console.log(id);
 
 // https://api.themoviedb.org/3/movie/634649?api_key=12890aac4bd3d481725b4e373193a5bf&language=en-US
 // https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
@@ -9,15 +8,14 @@ let IMAGE_URL = "http://image.tmdb.org/t/p/w500/";
 $.getJSON("/api/single-movie/" + id)
   .then((movie) => {
     $("#movie-details").hide();
-    console.log(IMAGE_URL + movie.poster_path);
     $("#title").text(movie.title);
     $("#tagline").text(movie.tagline);
-    $("#poster").append(
-      `<img src="${IMAGE_URL}${movie.poster_path}" alt="${movie.title}" />`
-    );
+    $("#poster").attr({
+      src: IMAGE_URL + movie.poster_path,
+      alt: "Poster for " + movie.title
+    });
     $("#year").text(movie.release_date.substr(0, 4));
     movie.genres.forEach((genre) => {
-      console.log(genre.name);
       $("#genres").append(`<span><em>${genre.name} | </em></span>`);
     });
     $("#synopsis").text(movie.overview);
@@ -28,3 +26,23 @@ $.getJSON("/api/single-movie/" + id)
   .then(() => {
     $("#movie-details").fadeIn();
   });
+
+$("#comment-form").submit((e) => {
+  e.preventDefault();
+  console.log($("#comment").val())
+  $.post(`/movies/${id}`, { comment: $("#comment").val() })
+    .then((response) => {
+      console.log(response);
+      const html =
+      `
+    <div>
+      <p><strong>${response.comment}</strong></p>
+      <p>${response.created_at}</p>
+    </div>
+      `
+      $("#comment-body").prepend(html)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
